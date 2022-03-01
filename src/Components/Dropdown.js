ed4b9d01074e09924fac476dec2677ca1815a622
osamahannan <83069516+osamahannan@users.noreply.github.com>
2022-03-01 23:08:59 +0530

    added arrow up button and and added dropdown functionality

--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react'
-import dropdownbtn from "../assets/dropdown-btn.png";
+import arrowdown from "../assets/arrow-down.png";
+import arrowup from "../assets/arrow-up.png";
 
 const Dropdown = () => {
 
     const userData = [{ name: "DC United" }, { name: "Manchester United" }, { name: "LA Galaxy" }]
 
     const [company, setCompany] = useState(userData);
+    const [showBox, setShowBox] = useState(false);
 
     useEffect(() => {
         setCompany(userData);
@@ -13,7 +15,6 @@ const Dropdown = () => {
 
     const handleChange = (e) => {
         const { name, checked } = e.target;
-        console.log(e.target.value);
         if (name === "allSelect") {
             let tempUser = company.map(user => {
                 return {
@@ -30,34 +31,37 @@ const Dropdown = () => {
 
     return (
         <div className='dropdown'>
-            <div className="dropdown-btn">
+            <div className="dropdown-btn" onClick={() => setShowBox(!showBox)}>
                 Company({company.length})
-                <img src={dropdownbtn} alt="dropdown" />
+                {showBox ? <img src={arrowup} alt="arrowup" className='arrowup' /> : <img src={arrowdown} alt="dropdown" className='arrowdown' />}
+
             </div>
-            <div className="dropdown-content">
-                <div className="form-check">
-                    <input
-                        type="checkbox"
-                        className='check-input'
-                        name="allSelect"
-                        onChange={handleChange}
-                        checked={company.filter((user) => user?.isChecked !== true).length < 1}
-                    />
-                    <label className='form-label'>Select all</label>
-                </div>
-                {company.map((item) => (
+            {showBox ?
+                <div className="dropdown-content">
                     <div className="form-check">
                         <input
                             type="checkbox"
                             className='check-input'
-                            name={item.name}
+                            name="allSelect"
                             onChange={handleChange}
-                            checked={item?.isChecked || false}
+                            checked={company.filter((user) => user?.isChecked !== true).length < 1}
                         />
-                        <label className='form-label'>{item.name}</label>
+                        <label className='form-label'>Select all</label>
                     </div>
-                ))}
-            </div>
+                    {company.map((item) => (
+                        <div className="form-check">
+                            <input
+                                type="checkbox"
+                                className='check-input'
+                                name={item.name}
+                                onChange={handleChange}
+                                checked={item?.isChecked || false}
+                            />
+                            <label className='form-label'>{item.name}</label>
+                        </div>
+                    ))}
+                </div> : ""
+            }
         </div>
     )
 }
