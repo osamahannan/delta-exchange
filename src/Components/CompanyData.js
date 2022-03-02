bdd1680a90c4ccd510cf865373390e7858466292
osamahannan <83069516+osamahannan@users.noreply.github.com>
2022-03-02 10:05:44 +0530

    added company details

---
+++ src/Components/CompanyData.js
@@ -0,0 +1,20 @@
+export const CompanyData = [
+    {
+        name: "Wayne Rooney",
+        Company: "DC United",
+        Status: "Active",
+        Notes: "ManUtd Highest Scorer"
+    },
+    {
+        name: "Ryan Giggs",
+        Company: "Manchester United",
+        Status: "Closed",
+        Notes: "Most matches played"
+    },
+    {
+        name: "Zlatan Ibrahimovic",
+        Company: "LA Galaxy",
+        Status: "Active",
+        Notes: "I am Zlatan"
+    },
+]
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Dropdown from './Dropdown'
+import { CompanyData } from './CompanyData'
+import deletebtn from "../assets/delete.png";
 
 const Home = () => {
 
@@ -10,6 +12,31 @@ const Home = () => {
                 <button className='btn add'>Add Members +</button>
             </div>
             <Dropdown />
+            <div className="heading-tags">
+                <div className="label">
+                    <input type="checkbox" />
+                    <h3>Name</h3>
+                </div>
+                <h3>Company</h3>
+                <h3>Status</h3>
+                <h3>Last Updated</h3>
+                <h3>Notes</h3>
+            </div>
+            {CompanyData.map((data) => (
+                <div className={`heading-tags ${data.Status}`}>
+                    <div className="label">
+                        <input type="checkbox" />
+                        <div>{data.name}</div>
+                    </div>
+                    <span>{data.Company}</span>
+                    <span>{data.Status}</span>
+                    <span>Last Updated</span>
+                    <span className='deletebtn'>
+                        {data.Notes}
+                        <img src={deletebtn} alt="delete" />
+                    </span>
+                </div>
+            ))}
         </div>
     )
 }
